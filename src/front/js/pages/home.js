c24fadf5a4e045b888d07a9712f1544b487f2bb1
mauriciio89 <maugalvez89@gmail.com>
2022-10-30 01:55:01 +0000

    footer

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,22 +4,23 @@ import ROUTINENATION from "../../img/ROUTINE NATION 2.png";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="container-fluid ms-auto">
-			<p>
-				<img src={ROUTINENATION} width={500} height={500} alt="..."/>
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="container-fluid ms-auto row-3">
+      <p>
+        <img src={ROUTINENATION} width={1000} height={700} alt="..." />
+      </p>
+      <div className="alert alert-info">
+        {store.message ||
+          "Loading message from the backend (make sure your python backend is running)..."}
+      </div>
+      <p>
+        This boilerplate comes with lots of documentation:{" "}
+        <a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
+          Read documentation
+        </a>
+      </p>
+    </div>
+  );
 };
