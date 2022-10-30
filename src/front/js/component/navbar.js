8ba6a85f3c632b7e57bf04ebc03020f37a96adb4
mauriciio89 <maugalvez89@gmail.com>
2022-10-30 02:53:55 +0000

    logo navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LOGO from "../../img/logo3.png";
 
 export const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-black">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          Routine Nation
+          <img src={LOGO} />
         </a>
         <button
           class="navbar-toggler"
